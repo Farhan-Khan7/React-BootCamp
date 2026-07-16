7ddcb246e19d16145ea666841380db55fa1e0407
Farhan-Khan7 <fk5518744@gmail.com>
2026-07-16 21:13:17 +0530

    I have done

---
+++ app.js
@@ -0,0 +1,11 @@
+export const myDetails = {
+    firstName : "Farhan",
+    lastName : "Khan",
+    Collage : "Dropped Out",
+    Age : "19 years old",
+    Siblings : 1,
+    passion : "FDE Engineer",
+}
+
+
+// export default myDetails; 
---
+++ script.js
@@ -0,0 +1,14 @@
+import {myDetails} from "./app.js";
+
+
+const importMyDetails = {
+    firstName : myDetails.firstName,
+    lastName : myDetails.lastName,
+    Collage : myDetails.Collage,
+    Age : myDetails.Age,
+    Siblings : myDetails.Siblings,
+    passion : myDetails.passion
+}
+
+console.log(importMyDetails)
+console.log(myDetails)
