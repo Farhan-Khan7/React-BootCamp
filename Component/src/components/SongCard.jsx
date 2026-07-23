bcf490d8ee1bc220046166a881e91e2aba0ccbd4
Farhan-Khan7 <fk5518744@gmail.com>
2026-07-24 02:00:48 +0530

    I have done components

--- Component/src/components/SongCard.jsx
+++ Component/src/components/SongCard.jsx
@@ -1,56 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SongCard = () => {
+  const [control , setContorls] = useState(false)
   const songs = [
     {
       banner:
         "https://i.pinimg.com/1200x/97/99/cd/9799cd23ade3032b6c5aa2bf832940f6.jpg",
       movie: "Fifty Shades Freed",
       song: "For You",
+      play : control
     },
     {
       banner:
         "https://i.pinimg.com/1200x/ad/03/b2/ad03b251be30ab415f969dd867fbcf81.jpg",
       movie: "Awarapan",
       song: "Toh Phir Aao",
+      play : control
     },
     {
       banner:
         "https://i.pinimg.com/736x/b7/ab/cc/b7abcc7f28e38117e80915720be4309a.jpg",
       movie: "Kabir Singh",
       song: "Kaise Hua",
+      play : control
     },
     {
       banner:
         "https://i.pinimg.com/736x/4f/96/6a/4f966ad7ccb496a1b9db7254a653cbcb.jpg",
       movie: "Shershaah",
       song: "Raataan Lambiyan",
+      play : control
     },
     {
       banner:
         "https://i.pinimg.com/736x/81/60/3b/81603b27e000c8847e0fd997233de0cc.jpg",
       movie: "Tamasha",
       song: "Agar Tum Saath Ho",
+      play : control
     },
     {
       banner:
         "https://i.pinimg.com/736x/3a/93/16/3a9316f344b33fa8c59e54bf5cf374c0.jpg",
       movie: "Jab We Met",
       song: "Tum Se Hi",
+      play : control
     },
     {
       banner:
         "https://i.pinimg.com/1200x/a8/33/d0/a833d02ad5db9e66078a140762504092.jpg",
       movie: "Animal",
       song: "Satranga",
+      play : control
     },
     {
       banner:
         "https://i.pinimg.com/1200x/47/1d/16/471d1658ef5ad8fef1998024731a1017.jpg",
       movie: "Bhediya",
       song: "Apna Bana Le",
+      play : control
     },
   ];
+  
 
   return songs.map((item, idx) => (
     <div className="w-70 h-110 bg-gray-500 rounded-xl opacity-55 flex flex-col justify-start px-4 py-3.5">
@@ -66,10 +76,10 @@ const SongCard = () => {
         </div>
         <h5 className="text-xs text-cyan-100">-0:27</h5>
       </div>
-      <div className="w-full flex items-center justify-center gap-8 text-xl text-white mt-6">
+      <div className="w-full flex items-center justify-center gap-8 text-xl text-white mt-6 cursor-pointer">
         <i className="ri-star-line"></i>
         <i className="ri-skip-back-fill text-2xl"></i>
-        <i className="ri-pause-large-fill text-2xl"></i>
+        {item.play ? <i onClick={()=>setContorls(false)} className="ri-pause-large-fill text-2xl"></i> : <i onClick={()=>setContorls(true)} class="ri-play-large-fill"></i>}
         <i className="ri-skip-forward-fill text-2xl"></i>
         <i className="ri-download-2-line"></i>
       </div>
